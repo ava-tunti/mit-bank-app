5b51aaee5efc2f4db1287f9f29f0e71f539f7ab1
Ava Tuntikanokporn <150287550+ava-tunti@users.noreply.github.com>
2024-08-13 01:05:20 -0700

    Update dal.js

--- dal.js
+++ dal.js
@@ -76,76 +76,157 @@
 
 
 // module.exports = { create, findOne, find, update, all };
+//--
+// const MongoClient = require('mongodb').MongoClient;
+// const url = process.env.MONGODB_URI || "mongodb+srv://avaananda:3qRUmG3QkMbFMx2T@fullbankapp.pr2f6.mongodb.net/?retryWrites=true&w=majority&appName=fullbankapp" || "mongodb://localhost:27017";
+// let db = null;
+
+// // // connect to mongo
+// // async function connectToDatabase() {
+// //     if (!db) {
+// //         const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+// //         console.log("Connected successfully to db server");
+// //         db = client.db('fullbankapp');
+// //     }
+// // }
+// // Connect to MongoDB
+// async function connectToDatabase() {
+//   try {
+//     const client = await MongoClient.connect(url, {
+//       // useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       tls: true,
+//       ssl: true, // Explicitly enable SSL
+//       sslValidate: true // Validate the server certificate
+//     });
+//     db = client.db("fullbankapp");
+//     console.log("Connected to MongoDB");
+//   } catch (err) {
+//     console.error(err);
+//     throw err; 
+//   }
+// }
+
+// // create user account using the collection.insertOne function
+// async function create(name, email, password) {
+//     await connectToDatabase();
+//     const collection = db.collection('users');
+//     const doc = { name, email, password, balance: 0 };
+//     return collection.insertOne(doc);
+// }
+
+// // find user account
+// async function find(email) {
+//     await connectToDatabase();
+//     const customers = db.collection('users').find({ email: email }).toArray();
+//     return customers;
+// }
+
+// // find user account
+// async function findOne(email) {
+//     await connectToDatabase();
+//     const customer = db.collection('users').findOne({ email: email });
+//     return customer;
+// }
+
+// // update - deposit/withdraw amount
+// async function update(email, amount) {
+//     await connectToDatabase();
+//     const result = db.collection('users').findOneAndUpdate(
+//         { email: email },
+//         { $inc: { balance: amount } },
+//         { returnOriginal: false }
+//     );
+//     return result;
+// }
+
+// // return all users by using the collection.find method
+// async function all() {
+//     await connectToDatabase();
+//     const customers = db.collection('users').find({}).toArray();
+//     return customers;
+// }
+
+// module.exports = { create, findOne, find, update, all };
+//--
 
 const MongoClient = require('mongodb').MongoClient;
-const url = process.env.MONGODB_URI || "mongodb+srv://avaananda:3qRUmG3QkMbFMx2T@fullbankapp.pr2f6.mongodb.net/?retryWrites=true&w=majority&appName=fullbankapp" || "mongodb://localhost:27017";
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
 let db = null;
 
-// // connect to mongo
-// async function connectToDatabase() {
-//     if (!db) {
-//         const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-//         console.log("Connected successfully to db server");
-//         db = client.db('fullbankapp');
-//     }
-// }
-// Connect to MongoDB
-async function connectToDatabase() {
-  try {
-    const client = await MongoClient.connect(url, {
-      // useNewUrlParser: true,
-      useUnifiedTopology: true,
-      tls: true,
-      ssl: true, // Explicitly enable SSL
-      sslValidate: true // Validate the server certificate
-    });
-    db = client.db("fullbankapp");
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.error(err);
-    throw err; 
-  }
-}
+// connect to mongo
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+    console.log("Connected successfully to db server");
+
+    // connect to myproject database
+    db = client.db('fullbank');
+});
 
 // create user account using the collection.insertOne function
-async function create(name, email, password) {
-    await connectToDatabase();
-    const collection = db.collection('users');
-    const doc = { name, email, password, balance: 0 };
-    return collection.insertOne(doc);
+function create(name, email, password) {
+    // TODO: populate this function based off the video
+    return new Promise((resolve, reject) => {
+        const collection = db.collection('users');
+        const doc = {name, email, password, balance:0};
+        collection.insertOne(doc, {w:1}, function(err, result) {
+            err ? reject(err) : resolve(doc);
+        });
+    }) 
+}
+
+// find user account 
+function find(email) {
+    return new Promise((resolve, reject) => {
+        const customers = db
+            .collection('users')
+            .find({ email: email })
+            .toArray(function (err, docs) {
+                err ? reject(err) : resolve(docs);
+            });
+    })
 }
 
 // find user account
-async function find(email) {
-    await connectToDatabase();
-    const customers = db.collection('users').find({ email: email }).toArray();
-    return customers;
-}
-
-// find user account
-async function findOne(email) {
-    await connectToDatabase();
-    const customer = db.collection('users').findOne({ email: email });
-    return customer;
+function findOne(email) {
+    return new Promise((resolve, reject) => {
+        const customers = db
+            .collection('users')
+            .findOne({ email: email })
+            .then((doc) => resolve(doc))
+            .catch((err) => reject(err));
+    })
 }
 
 // update - deposit/withdraw amount
-async function update(email, amount) {
-    await connectToDatabase();
-    const result = db.collection('users').findOneAndUpdate(
-        { email: email },
-        { $inc: { balance: amount } },
-        { returnOriginal: false }
-    );
-    return result;
+function update(email, amount) {
+    return new Promise((resolve, reject) => {
+        const customers = db
+            .collection('users')
+            .findOneAndUpdate(
+                { email: email },
+                { $inc: { balance: amount } },
+                { returnOriginal: false },
+                function (err, documents) {
+                    err ? reject(err) : resolve(documents);
+                }
+            );
+
+
+    });
 }
 
 // return all users by using the collection.find method
-async function all() {
-    await connectToDatabase();
-    const customers = db.collection('users').find({}).toArray();
-    return customers;
+function all() {
+    // TODO: populate this function based off the video
+    return new Promise((resolve, reject) => {
+        const customers = db
+            .collection('users')
+            .find({})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+            });
+    })
 }
 
-module.exports = { create, findOne, find, update, all };
 
+module.exports = { create, findOne, find, update, all };
